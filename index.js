aa55f51abdf3e1eafc4ccc59cd727589b8e5adc7
Xotic750 <xotic750@gmail.com>
2017-07-13 00:20:53 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,78 +1,47 @@
 /**
- * @file
- * <a href="https://travis-ci.org/Xotic750/is-set-x"
- * title="Travis status">
- * <img src="https://travis-ci.org/Xotic750/is-set-x.svg?branch=master"
- * alt="Travis status" height="18">
- * </a>
- * <a href="https://david-dm.org/Xotic750/is-set-x"
- * title="Dependency status">
- * <img src="https://david-dm.org/Xotic750/is-set-x.svg"
- * alt="Dependency status" height="18"/>
- * </a>
- * <a
- * href="https://david-dm.org/Xotic750/is-set-x#info=devDependencies"
- * title="devDependency status">
- * <img src="https://david-dm.org/Xotic750/is-set-x/dev-status.svg"
- * alt="devDependency status" height="18"/>
- * </a>
- * <a href="https://badge.fury.io/js/is-set-x" title="npm version">
- * <img src="https://badge.fury.io/js/is-set-x.svg"
- * alt="npm version" height="18">
- * </a>
- *
- * Detect whether or not an object is an ES6 SET.
- *
- * @version 1.2.0
+ * @file Detect whether or not an object is an ES6 SET.
+ * @version 1.3.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module is-set-x
  */
 
-/* eslint strict: 1 */
+'use strict';
 
-/* global module */
+var isObjectLike;
+var getSize;
 
-;(function () { // eslint-disable-line no-extra-semi
+if (typeof Set === 'function') {
+  try {
+    getSize = Object.getOwnPropertyDescriptor(Set.prototype, 'size').get;
+    getSize = typeof getSize.call(new Set()) === 'number' && getSize;
+    isObjectLike = require('is-object-like-x');
+  } catch (ignore) {}
+}
 
-  'use strict';
-
-  var isObjectLike;
-  var getSize;
-
-  if (typeof Set === 'function') {
-    try {
-      getSize = Object.getOwnPropertyDescriptor(Set.prototype, 'size').get;
-      getSize = typeof getSize.call(new Set()) === 'number' && getSize;
-      isObjectLike = require('is-object-like-x');
-    } catch (ignore) {
-      getSize = null;
-    }
+/**
+ * Determine if an `object` is a `Set`.
+ *
+ * @param {*} object - The object to test.
+ * @returns {boolean} `true` if the `object` is a `Set`,
+ *  else false`.
+ * @example
+ * var isSet = require('is-set-x');
+ * var s = new Set();
+ *
+ * isSet([]); // false
+ * isSet(true); // false
+ * isSet(s); // true
+ */
+module.exports = function isSet(object) {
+  if (Boolean(getSize) === false || isObjectLike(object) === false) {
+    return false;
   }
 
-  /**
-   * Determine if an `object` is a `Set`.
-   *
-   * @param {*} object The object to test.
-   * @return {boolean} `true` if the `object` is a `Set`,
-   *  else false`
-   * @example
-   * var isSet = require('is-set-x');
-   * var s = new Set();
-   *
-   * isSet([]); // false
-   * isSet(true); // false
-   * isSet(s); // true
-   */
-  module.exports = function isSet(object) {
-    if (!getSize || !isObjectLike(object)) {
-      return false;
-    }
-    try {
-      return typeof getSize.call(object) === 'number';
-    } catch (ignore) {}
+  try {
+    return typeof getSize.call(object) === 'number';
+  } catch (ignore) {}
 
-    return false;
-  };
-}());
+  return false;
+};
--- tests/spec/test.js
+++ tests/spec/test.js
@@ -1,72 +1,56 @@
-/* jslint maxlen:80, es6:true, white:true */
+'use strict';
 
-/* jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
-   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
-   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-   es3:false, esnext:true, plusplus:true, maxparams:1, maxdepth:2,
-   maxstatements:12, maxcomplexity:4 */
-
-/* eslint strict: 1, max-lines: 1, symbol-description: 1, max-nested-callbacks: 1,
-   max-statements: 1 */
-
-/* global JSON:true, expect, module, require, describe, xit, it, returnExports */
-
-;(function () { // eslint-disable-line no-extra-semi
-
-  'use strict';
-
-  var isSet;
-  if (typeof module === 'object' && module.exports) {
-    require('es5-shim');
-    require('es5-shim/es5-sham');
-    if (typeof JSON === 'undefined') {
-      JSON = {};
-    }
-    require('json3').runInContext(null, JSON);
-    require('es6-shim');
-    var es7 = require('es7-shim');
-    Object.keys(es7).forEach(function (key) {
-      var obj = es7[key];
-      if (typeof obj.shim === 'function') {
-        obj.shim();
-      }
-    });
-    isSet = require('../../index.js');
-  } else {
-    isSet = returnExports;
+var isSet;
+if (typeof module === 'object' && module.exports) {
+  require('es5-shim');
+  require('es5-shim/es5-sham');
+  if (typeof JSON === 'undefined') {
+    JSON = {};
   }
-
-  var hasSet = typeof Set === 'function';
-  if (hasSet) {
-    try {
-      var getSize = Object.getOwnPropertyDescriptor(
-        Object.getPrototypeOf(new Set()),
-        'size'
-      ).get;
-      if (typeof getSize.call(new Set()) !== 'number') {
-        throw new TypeError('not a number');
-      }
-    } catch (ignore) {
-      hasSet = false;
+  require('json3').runInContext(null, JSON);
+  require('es6-shim');
+  var es7 = require('es7-shim');
+  Object.keys(es7).forEach(function (key) {
+    var obj = es7[key];
+    if (typeof obj.shim === 'function') {
+      obj.shim();
     }
-  }
-
-  var ifHasSet = hasSet ? it : xit;
-
-  describe('isSet', function () {
-    it('basic', function () {
-      expect(isSet()).toBe(false);
-      expect(isSet(undefined)).toBe(false);
-      expect(isSet(null)).toBe(false);
-      expect(isSet(1)).toBe(false);
-      expect(isSet(true)).toBe(false);
-      expect(isSet('abc')).toBe(false);
-      expect(isSet([])).toBe(false);
-      expect(isSet({})).toBe(false);
-    });
-
-    ifHasSet('hasSet', function () {
-      expect(isSet(new Set())).toBe(true);
-    });
   });
-}());
+  isSet = require('../../index.js');
+} else {
+  isSet = returnExports;
+}
+
+var hasSet = typeof Set === 'function';
+if (hasSet) {
+  try {
+    var getSize = Object.getOwnPropertyDescriptor(
+      Object.getPrototypeOf(new Set()),
+      'size'
+    ).get;
+    if (typeof getSize.call(new Set()) !== 'number') {
+      throw new TypeError('not a number');
+    }
+  } catch (ignore) {
+    hasSet = false;
+  }
+}
+
+var ifHasSet = hasSet ? it : xit;
+
+describe('isSet', function () {
+  it('basic', function () {
+    expect(isSet()).toBe(false);
+    expect(isSet(undefined)).toBe(false);
+    expect(isSet(null)).toBe(false);
+    expect(isSet(1)).toBe(false);
+    expect(isSet(true)).toBe(false);
+    expect(isSet('abc')).toBe(false);
+    expect(isSet([])).toBe(false);
+    expect(isSet({})).toBe(false);
+  });
+
+  ifHasSet('hasSet', function () {
+    expect(isSet(new Set())).toBe(true);
+  });
+});
