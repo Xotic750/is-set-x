b50d3c614f9c95c394f9be902bc784a9b0dcd20e
Graham Fairweather <xotic750@gmail.com>
2019-07-26 16:52:46 +0200

    :bookmark: v2.0.10
    Auto build

--- dist/is-set-x.js
+++ dist/is-set-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-25T22:40:39.688Z",
+  "date": "2019-07-26T14:52:39.872Z",
   "describe": "",
   "description": "Detect whether or not an object is an ES6 SET.",
   "file": "is-set-x.js",
   "hash": "eb7528fccfe137fa37cc",
   "license": "MIT",
-  "version": "2.0.9"
+  "version": "2.0.10"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
