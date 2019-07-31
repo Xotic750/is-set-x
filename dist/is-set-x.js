d7d0d7368e880ccf5d14bcd98a1a1a765795fb5f
Graham Fairweather <xotic750@gmail.com>
2019-07-31 22:18:22 +0200

    :bookmark: v2.0.12
    Auto build

--- dist/is-set-x.js
+++ dist/is-set-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-07-31T18:00:21.257Z",
+  "date": "2019-07-31T20:18:16.984Z",
   "describe": "",
   "description": "Detect whether or not an object is an ES6 SET.",
   "file": "is-set-x.js",
