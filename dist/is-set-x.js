9038502e699bb34bfd42f8d88e1e10b4f013bf09
Graham Fairweather <xotic750@gmail.com>
2019-09-05 02:04:33 +0200

    Maintenance

--- dist/is-set-x.js
+++ dist/is-set-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-30T22:38:27.541Z",
+  "date": "2019-09-05T00:04:26.286Z",
   "describe": "",
   "description": "Detect whether or not an object is an ES6 SET.",
   "file": "is-set-x.js",
